4c3d43fbaafc0ff3d3631c76c2e2f25306e7ebff
SireJeff <61094553+Mshiri@users.noreply.github.com>
2023-10-26 22:34:19 +0330

    modified:   EX1/pmB2.py

--- EX1/pmB2.py
+++ EX1/pmB2.py
@@ -1,8 +1,21 @@
-a=int(input())
-b=int(input())
-s=int(input())
-su=a+b
-if s==su:
-    print(f"{su}\nYES")
-else :
-    print(f"{su}\nNO")
+# Input two integers
+num1 = int(input())
+num2 = int(input())
+
+# Add the two integers using logical operators
+result = num1
+carry = 0
+
+while num2 != 0:
+    carry = result & num2
+    result = result ^ num2
+    num2 = carry << 1
+
+# Input a third integer
+third_num = int(input())
+
+# Check if the sum of the first two integers matches the third
+if result == third_num:
+    print(f"{result}\nYES")
+else:
+    print(f"{result}\nNO")
