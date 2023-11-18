08ee6d5f8079d539893e25b6152eec4dcb52e195
SireJeff <61094553+Mshiri@users.noreply.github.com>
2023-11-18 21:27:53 +0330

    modified:   EX3/rMB2.py 	modified:   EX3/tMB3.py

--- EX3/rMB2.py
+++ EX3/rMB2.py
@@ -1,36 +1,20 @@
-def extract_domains(emails):
-    domains = set()
-    email_count = {}
-    
-    for email in emails:
-        # جدا کردن دامنه از ایمیل
-        domain = email.split('@')[-1]
-        
-        # اگر این دامنه قبلاً در خروجی ظاهر نشده باشد، اضافه کن
-        if domain not in email_count:
-            domains.add(domain)
+import re
+def extract_domain(email):
+    # Match any characters after the last dot in the domain
+    pattern = r"@([A-Za-z0-9.-]+)\.([A-Za-z0-9.-]+)"
+    match = re.findall(pattern, email)
+    return '.'.join(match[0])
 
-        # افزایش تعداد ایمیل‌های این دامنه
-        email_count[domain] = email_count.get(domain, 0) + 1
 
-    return sorted(domains,reverse=True)
 
-def main():
-    # خواندن تعداد ایمیل‌ها
-    n = int(input())
+n = int(input())
+domains = set()
+for i in range(n):
+    email = input()
+    domain = extract_domain(email)
+    domains.add(domain)
 
-    # خواندن ایمیل‌ها
-    emails = []
-    for _ in range(n):
-        email = input()
-        emails.append(email)
+jeff =sorted(domains, reverse=True)
+for domain in reversed(jeff):
+    print(domain)
 
-    # استخراج دامنه‌ها با ترتیب
-    domains = extract_domains(emails)
-
-    # چاپ دامنه‌ها
-    for domain in reversed(domains):
-        print(domain)
-
-if __name__ == "__main__":
-    main()
--- EX3/tMB3.py
+++ EX3/tMB3.py
@@ -1,10 +1,24 @@
-import codecs
 def format_text(input_text):
-    # Remove # after @
-    formatted_text = input_text.replace('@#', '@')
+    # Split the text into a list of characters
+    char_list = list(input_text)
 
-    # Remove specified # characters
-    formatted_text = formatted_text.replace('#', '')
+    # Iterate through the characters and remove extra '#' after '@'
+    i = 0
+    while i < len(char_list):
+        if char_list[i] == '@':
+            j = i + 1
+            while j < len(char_list):
+                if char_list[j] == '#':
+                    char_list.pop(j)
+                    break
+                else:
+                    j += 1
+            i += 1
+        else:
+            i += 1
+
+    # Join the characters back into a string
+    formatted_text = ''.join(char_list)
 
     # Remove extra spaces
     formatted_text = ' '.join(formatted_text.split())
@@ -12,11 +26,15 @@ def format_text(input_text):
     # Handle new lines after "n\"
     formatted_text = formatted_text.replace('n\\', '\n')
 
+    # Replace escape sequences
+    formatted_text = formatted_text.replace("\\n", "\n")
+    formatted_text = formatted_text.replace("\\t", "\t")
+
     return formatted_text
 
 # Get input from the user
 user_input = input()
+
 # Call the function and print the result
 formatted_text = format_text(user_input)
-user_inputt = codecs.decode(formatted_text, 'unicode_escape')
-print(f"Formatted Text: {user_inputt}")
+print(f"Formatted Text: {formatted_text}")
