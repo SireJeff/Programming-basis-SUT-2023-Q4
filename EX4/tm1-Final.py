a9e887dcd3f2f3049c7081a69d57a3bf173b3db9
SireJeff <61094553+Mshiri@users.noreply.github.com>
2023-12-21 15:27:33 +0330

    modified:   EX4/tm1-Final.py

--- EX4/tm1-Final.py
+++ EX4/tm1-Final.py
@@ -1,66 +1,40 @@
 import os
+import pandas as pd
+import numpy as np
 
-def transpose(matrix):
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-
-def matrix_multiply(mat1, mat2):
-    return [
-        [sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)] 
-        for row in mat1
-    ]
-
-def determinant(matrix):
-    if len(matrix) == 1:
-        return matrix[0][0]
-    elif len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    else:
-        det = 0
-        for i in range(len(matrix)):
-            submatrix = [row[:i] + row[i+1:] for row in matrix[1:]]
-            det += ((-1) ** i) * matrix[0][i] * determinant(submatrix)
-        return det
-
-def inverse(matrix):
-    det = determinant(matrix)
-    if det == 0:
-        return None  
-    n = len(matrix)
-    adjugate = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            minor = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
-            cofactor = ((-1) ** (i + j)) * determinant(minor)
-            adjugate[j][i] = cofactor
-    scalar = 1 / det
-    return [[elem * scalar for elem in row] for row in adjugate]
-
-def process_input(content):
-    
-    num_matrices, matrix_size = map(int, content[0].split())
-    
-    matrices_list = []
-    for i in range(1, 1 + num_matrices * matrix_size, matrix_size):
-        matrix = [list(map(int, line.split())) for line in content[i:i+matrix_size]]
-        matrices_list.append(matrix)
-
+def calculate_and_print_inverted_matrix(num_matrices, matrix_size, matrices_list):
     max_det = float('-inf')
     max_det_indices = None
+
     for i in range(num_matrices):
         for j in range(i + 1, num_matrices):
-            det_ij = determinant(matrix_multiply(matrices_list[i], matrices_list[j]))
+            det_ij = np.linalg.det(np.dot(matrices_list[i], matrices_list[j]))
             if det_ij > max_det:
                 max_det = det_ij
                 max_det_indices = (i, j)
-    if determinant(matrices_list[max_det_indices[0]])==determinant(matrices_list[max_det_indices[1]]):
-        result_matrix=matrix_multiply(matrices_list[max_det_indices[0]], matrices_list[max_det_indices[1]])
-    elif determinant(matrices_list[max_det_indices[0]])>determinant(matrices_list[max_det_indices[1]]):
-        result_matrix = matrix_multiply(matrices_list[max_det_indices[0]], matrices_list[max_det_indices[1]])
-    else:
-        result_matrix = matrix_multiply(matrices_list[max_det_indices[1]], matrices_list[max_det_indices[0]])
-    inverted_matrix = inverse(result_matrix)
 
+    matrix_a_det = np.linalg.det(matrices_list[max_det_indices[0]])
+    matrix_b_det = np.linalg.det(matrices_list[max_det_indices[1]])
+
+    if matrix_a_det == matrix_b_det:
+        result_matrix = np.dot(matrices_list[max_det_indices[0]], matrices_list[max_det_indices[1]])
+    elif matrix_a_det > matrix_b_det:
+        result_matrix = np.dot(matrices_list[max_det_indices[0]], matrices_list[max_det_indices[1]])
+    else:
+        result_matrix = np.dot(matrices_list[max_det_indices[1]], matrices_list[max_det_indices[0]])
+
+    inverted_matrix = np.linalg.inv(result_matrix)
     return "\n".join(" ".join(f"{elem:.3f}" for elem in row) for row in inverted_matrix)
+
+def process_input(content):
+    num_matrices, matrix_size = map(int, content[0].split())
+
+    matrices_list = []
+    for i in range(1, 1 + num_matrices * matrix_size, matrix_size):
+        matrix = [list(map(int, line.split())) for line in content[i:i + matrix_size]]
+        matrices_list.append(np.array(matrix))
+
+    return calculate_and_print_inverted_matrix(num_matrices, matrix_size, matrices_list)
 
 def main():
     input_directory = "./in/"
