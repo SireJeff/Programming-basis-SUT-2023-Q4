998a57bd1f8ca392fd6c9d3d661b5869a112a109
SireJeff <61094553+Mshiri@users.noreply.github.com>
2023-12-22 16:28:14 +0330

    renamed:    EX4/tm1-Final.py -> EX4/tm1/tm1-Final.py 	modified:   EX4/tm4.py

--- EX4/tm4.py
+++ EX4/tm4.py
@@ -1,7 +1,7 @@
 class DoctorOffice:
     def __init__(self):
         self.patients = {}
-        self.visits = {}
+        self.visits = []
 
     def add_patient(self, id, name, family_name, age, height, weight):
         if id in self.patients:
@@ -18,7 +18,8 @@ class DoctorOffice:
             'family_name': family_name,
             'age': age,
             'height': height,
-            'weight': weight
+            'weight': weight,
+            'visits': []  # Initialize an empty list for visits
         }
         return "patient added successfully"
 
@@ -33,10 +34,11 @@ class DoctorOffice:
             return "error: invalid id"
         if not (9 <= beginning_time <= 18):
             return "error: invalid time"
-        if beginning_time in self.visits.values():
-            return "error: busy time"
-
-        self.visits[id] = beginning_time
+        for i in self.patients.keys():# Check if the time is already occupied for this patient
+            if beginning_time in self.patients[i]['visits']:
+                return "error: busy time"
+        self.patients[id]['visits'].append(beginning_time)
+        self.visits.append((id, beginning_time))
         return "visit added successfully!"
 
     def delete_patient(self, id):
@@ -45,39 +47,60 @@ class DoctorOffice:
 
         del self.patients[id]
         # Remove patient's visits as well
-        self.visits = {k: v for k, v in self.visits.items() if k != id}
+        self.visits = [(patient_id, time) for patient_id, time in self.visits if patient_id != id]
         return "patient deleted successfully!"
 
     def display_visit_list(self):
-        schedule = sorted(self.visits.items(), key=lambda x: x[1])
         result = "SCHEDULE:\n"
-        for id, time in schedule:
-            result += f"{time:02d}:00 ({self.patients[id]['name']}) ({self.patients[id]['family_name']})\n"
-        return result.strip()
+        
+        for i in range(len(self.visits)):
+         patient_id, time = self.visits[i]
+         patient_info = self.patients[patient_id]
+         if i+1==len(self.visits):
+            result += f"{time}:00 {patient_info['name']} {patient_info['family_name']}"
+         else:
+            result += f"{time}:00 {patient_info['name']} {patient_info['family_name']}\n"
 
+        return result.strip()
 
 def main():
     office = DoctorOffice()
+    commands = []
+    commandlist=["add","display","delete","exit"]
     while True:
-        command = input().split()
-        if command[0] == "add":
-            if command[1] == "patient":
-                print(office.add_patient(int(command[2]), command[3], command[4], int(command[5]), int(command[6]), int(command[7])))
-            elif command[1] == "visit":
-                print(office.add_visit(int(command[2]), int(command[3])))
-        elif command[0] == "display":
-            if command[1] == "patient":
-                print(office.display_patient(int(command[2])))
-            elif command[1] == "visit" and command[2] == "list":
-                print(office.display_visit_list())
-        elif command[0] == "delete":
-            if command[1] == "patient":
-                print(office.delete_patient(int(command[2])))
-        elif command[0] == "exit":
-            break
-        else:
+        try:
+            command = input().split()
+            commands.append(command)
+
+            if command[0] == "exit":
+                break
+        except Exception:
             print("invalid command")
+    
+    for command in commands:
+        if not command:
+            continue
+        execute_command(command, office)
 
 
+def execute_command(command, office):
+    if command[0] == "add":
+        if command[1] == "patient":
+            print(office.add_patient(int(command[2]), command[3], command[4], int(command[5]), int(command[6]), int(command[7])))
+        elif command[1] == "visit":
+            print(office.add_visit(int(command[2]), int(command[3])))
+    elif command[0] == "display":
+        if command[1] == "patient":
+            print(office.display_patient(int(command[2])))
+        elif command[1] == "visit" and command[2] == "list":
+            print(office.display_visit_list())
+    elif command[0] == "delete":
+        if command[1] == "patient":
+            print(office.delete_patient(int(command[2])))
+    elif command[0] == "exit":
+        pass  # The "exit" command is already handled
+    else:
+        print("invalid command")
+    
 if __name__ == "__main__":
     main()
