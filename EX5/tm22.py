fcb1ad43768e6ee16cbd6ef4713ece9e80d2c6b8
SireJeff <61094553+Mshiri@users.noreply.github.com>
2024-01-25 20:49:06 +0330

    renamed:    excercises 2/pmT1.py -> EX2/pmT1.py 	renamed:    excercises 2/pmT2.py -> EX2/pmT2.py 	renamed:    excercises 2/pmT3.py -> EX2/pmT3.py 	renamed:    excercises 2/pmT4.py -> EX2/pmT4.py 	renamed:    excercises 2/t4.py -> EX2/t4.py 	renamed:    EX 5/tm1.py -> EX5/tm1.py 	renamed:    EX 5/tm2.py -> EX5/tm2.py 	new file:   EX5/tm22.py 	renamed:    EX 5/tm3.py -> EX5/tm3.py 	new file:   EX5/tm4.py 	deleted:    output_file.txt

---
+++ EX5/tm22.py
@@ -0,0 +1,14 @@
+class jef:
+    __jeff="jeff pv"
+    _jeffo="jeff pctd"
+    def __init__(self):
+        self.wwtf="some attr"
+class seth(jef):
+    def __init__(self):
+        #print(self.__jeff)
+        print(self._jeffo)
+jef._jeffo="gtfo"
+jefff=jef()
+sethh=seth()
+jefff._jef__jeff="gtfo o my face mite"
+print(jefff._jef__jeff)
