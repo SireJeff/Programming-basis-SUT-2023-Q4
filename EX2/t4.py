a60899e0c27eac87b93da7e105c04817abe58e5b
Mohammad Hasan Shiri <61094553+SireJeff@users.noreply.github.com>
2024-01-25 20:54:17 +0330

    Delete EX2 directory

--- EX2/t4.py
+++
@@ -1,9 +0,0 @@
-def count_ways_to_make_change(amount, coins):
-    dp = [0] * (amount + 1)
-
-    for coin in coins:
-
-        for i in range(coin, amount + 1):
-
-            dp[i] += dp[i - coin]
-    return dp[amount]
