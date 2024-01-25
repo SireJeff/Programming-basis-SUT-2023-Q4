043cfe384c175e159fe2eca6cbf7ba345ab8c42e
Mohammad Hasan Shiri <61094553+SireJeff@users.noreply.github.com>
2024-01-25 20:52:13 +0330

    Delete EX3 directory

--- EX3/test2.py
+++
@@ -1,5 +0,0 @@
-import re
-decimal_number_str = input("Enter a decimal number: ")
-filtered_string = re.sub(r'[^0-9a-fA-F]', '', decimal_number_str)
-
-print("Hexadecimal Representation:", filtered_string)
